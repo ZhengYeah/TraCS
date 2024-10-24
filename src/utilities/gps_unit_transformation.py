b37797a907746e5bb1beb589d79b9197114076f2
郑烨 <zhengyeah@foxmail.com>
2024-10-24 14:00:55 -0400

    Finished all experiments

--- src/utilities/gps_unit_transformation.py
+++ src/utilities/gps_unit_transformation.py
@@ -1,13 +1,3 @@
-def gps_to_unit_square(x, y, x_min: float, x_max: float, y_min: float, y_max: float):
-    """
-    transform GPS coordinates to unit-square coordinates
-    """
-    assert len(x) == len(y)
-    x_length = x_max - x_min
-    y_length = y_max - y_min
-    return (x - x_min) / x_length, (y - y_min) / y_length
-
-
 def unit_square_to_gps(traj, x_min: float, x_max: float, y_min: float, y_max: float):
     """
     transform unit square coordinates to GPS coordinates
