18441091485c4c5c9b5588ede71cd9c7d0302fcf
ZhengYeah <179118481@qq.com>
2024-10-28 23:30:02 -0400

    Finished all experiments

--- experiments/other_figures_in_paper/strawman_vs_def3.py
+++ experiments/other_figures_in_paper/strawman_vs_def3.py
@@ -1,20 +1,39 @@
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-pi = math.pi
+pi = np.pi
+exp = np.e
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.size'] = 20
+plt.rcParams['font.family'] = 'serif'
+
 bins = 12
-epsilon = 2
 varphi = pi / 6
 
-
+epsilon = np.linspace(1.8, 10, 20, endpoint=True)
 # krr
-p = math.exp(epsilon) / (math.exp(epsilon) + bins - 1)
-s = (2*pi / bins)
-print(f"CDF: {p:.3f}, Angular: {s/pi:.3f} pi")
+krr_p = exp ** epsilon / (exp ** epsilon + bins - 1)
+krr_s = 2 * pi / bins
+krr_s = np.repeat(krr_s, len(epsilon))
 
 # circular mechanism
-p = 1 / (2*pi) * math.exp(epsilon/2)
-s = 2*pi * (math.exp(epsilon/2) - 1) / (math.exp(epsilon) - 1)
-l = (varphi - s / 2) / pi
-r = (varphi + s / 2) / pi
-proba_s = p * s
-print(f"CDF: {proba_s:.3f}, Angular: {s/pi:.3f} pi")
+tracs_p = 1 / (2*pi) * exp ** (epsilon/2)
+tracs_s = 2*pi * (exp ** (epsilon/2) - 1) / (exp ** epsilon - 1)
+proba_s = tracs_p * tracs_s
+
+# two y-axes plot
+plt.plot(epsilon, krr_s, label='Strawman', color='red', linestyle='--')
+plt.plot(epsilon, tracs_s, label='TraCS-D', color='red')
+plt.yticks(np.arange(0, pi+0.2, pi/4), ['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$'])
+plt.ylabel('Size')
+plt.twinx()
+plt.plot(epsilon, krr_p, label='Strawman', color='black')
+plt.plot(epsilon, proba_s, label='TraCS-D', color='black', linestyle='--')
+plt.ylabel('Probability')
+plt.xlabel(r'Privacy parameter $\varepsilon$')
+
+plt.savefig('strawman_vs_def3.pdf', bbox_inches='tight')
+plt.show()
+
+
