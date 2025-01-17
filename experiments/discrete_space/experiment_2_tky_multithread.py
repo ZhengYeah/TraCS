90e850bbef1cabfdd043674d0723b53b311f83ff
ZhengYeah <179118481@qq.com>
2025-01-17 08:28:52 -0500

    USENIX submission

--- experiments/discrete_space/experiment_2_tky_multithread.py
+++ experiments/discrete_space/experiment_2_tky_multithread.py
@@ -9,19 +9,21 @@ from multiprocessing import Pool
 
 pi = np.pi
 
-def load_chi():
-    with open(f"./CHI/chi_location_space.pkl", "rb") as f:
+def load_nyc():
+    with open(f"./TKY/tky_location_space.pkl", "rb") as f:
         location_space = pickle.load(f)
-    with open(f"./CHI/chi_trajectory.pkl", "rb") as f:
+    with open(f"./TKY/tky_trajectory.pkl", "rb") as f:
         trajectory = pickle.load(f)
+    # print(f"Length of trajectory: {len(trajectory)}")
+    # print(f"Length of location space: {len(location_space)}")
     return location_space, trajectory
 
 
 def process_trajectory(traj):
-    location_space, epsilon = load_chi()[0], 2
+    location_space, epsilon = load_nyc()[0], 4
     # perturbed trajectories
     perturbed_traj_tp = tp_perturb(traj, location_space, epsilon)
-    perturbed_traj_ngram = ngram_perturb(traj, location_space, epsilon, theta=0.75)
+    perturbed_traj_ngram = ngram_perturb(traj, location_space, epsilon, theta=0.8)
     perturbed_traj_tracs_d = tracs_d(traj, epsilon, pi / (pi + 1) * epsilon)
     perturbed_traj_tracs_c = tracs_c(traj, epsilon, epsilon / 2)
     # round the perturbed locations to the nearest location in the location space (TraCS)
@@ -32,8 +34,8 @@ def process_trajectory(traj):
         nearest_location_c = location_space[np.argmin(np.linalg.norm(location_space - perturbed_traj_tracs_c[i], axis=1))]
         perturbed_traj_tracs_c[i] = nearest_location_c
     # transform back to gps coordinates
-    # CHI: x_min: 41.60015, x_max: 41.99822, y_min: -87.9952, y_max: -87.50765
-    x_min, x_max, y_min, y_max = 41.60015, 41.99822, -87.9952, -87.50765
+    # TKY: x_min: 35.51076909, x_max: 35.86407116, y_min: 139.47087765, y_max: 139.908359
+    x_min, x_max, y_min, y_max = 35.51076909, 35.86407116, 139.47087765, 139.908359
     gps_traj = unit_square_to_gps(traj, x_min, x_max, y_min, y_max)
     gps_perturbed_traj_tp = unit_square_to_gps(perturbed_traj_tp, x_min, x_max, y_min, y_max)
     gps_perturbed_traj_ngram = unit_square_to_gps(perturbed_traj_ngram, x_min, x_max, y_min, y_max)
@@ -52,14 +54,14 @@ def process_trajectory(traj):
 
 
 if __name__ == '__main__':
-    location_space, trajectory = load_chi()
-    # first 100 and last 100 trajectories
-    trajectory = trajectory[:100]
+    location_space, trajectory = load_nyc()
+    # first 100 trajectories
+    trajectory = trajectory[:10]
     with Pool() as pool:
         error_list = pool.map(process_trajectory, trajectory)
     error_list = np.array(error_list)
     print(f"epsilon: 1, tp: {np.mean(error_list[:, 0])}, ngram: {np.mean(error_list[:, 1])}, tracs-d: {np.mean(error_list[:, 2])}, tracs-c: {np.mean(error_list[:, 3])}")
     # write to csv
-    with open(f"./results/experiment_2_chi.csv", "a") as f:
+    with open(f"./results/experiment_2_tky.csv", "a") as f:
         f.write("tp,ngram,tracs_d,tracs_c\n")
         f.write(f"{np.mean(error_list[:, 0])}, {np.mean(error_list[:, 1])}, {np.mean(error_list[:, 2])}, {np.mean(error_list[:, 3])}\n")
